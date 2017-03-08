4eafab6fc1595bf4650c20183a9789a17c7a9a5a
mariussm <marius.msm@gmail.com>
2017-03-08 07:39:32 +0100

    Added readme

--- Profile.ps1
+++ Profile.ps1
@@ -25,3 +25,25 @@ Import-Module (Join-Path (Split-Path -Parent -Path $profile) "FIMPowerShellModul
 if($env:USERPROFILE -and (pwd).Path.StartsWith("C:\Windows")) {
     cd $env:USERPROFILE
 }
+
+
+function Get-PowerShellProfileInstallScript
+{
+    [CmdletBinding()]
+    Param(
+        [Parameter(Mandatory=$false,
+                   ValueFromPipelineByPropertyName=$false,
+                   Position=0)]
+        $Branch = "master",
+
+        [Parameter(Mandatory=$false,
+                   ValueFromPipelineByPropertyName=$false,
+                   Position=1)]
+        $Repository = "mariussm/powershell-profile"
+    )
+
+    "Invoke-RestMethod -OutFile `$profile -Uri ""https://raw.githubusercontent.com/$Repository/$Branch/Profile.ps1""
+    Invoke-RestMethod -OutFile (Join-Path (Split-Path -Parent -Path `$profile) ""Cmdlets.ps1"") -Uri ""https://raw.githubusercontent.com/$Repository/$Branch/Cmdlets.ps1""
+    Invoke-RestMethod -OutFile (Join-Path (Split-Path -Parent -Path `$profile) ""FIMPowerShellModule.psm1"") -Uri ""https://raw.githubusercontent.com/$Repository/$Branch/Modules/FIMPowerShellModule.psm1""
+    . `$profile"
+}
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# powershell-profile
+# PowerShell profile used by Marius Solbakken
+
+Feel free to utilize stuff from this repo, but please mention my name if used when publishing.

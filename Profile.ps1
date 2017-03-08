de5403435b101c3e611e387d95df5b0c33924532
mariussm <marius.msm@gmail.com>
2017-03-08 07:30:45 +0100

    Added parameters to update cmdlets

--- Profile.ps1
+++ Profile.ps1
@@ -1,13 +1,27 @@
 ﻿function Update-PowerShellProfile
 {
     [CmdletBinding()]
-    Param()
+    Param(
+        [Parameter(Mandatory=$false,
+                   ValueFromPipelineByPropertyName=$false,
+                   Position=0)]
+        $Branch = "master",
 
-    Invoke-RestMethod -OutFile $profile -Uri "https://raw.githubusercontent.com/mariussm/powershell-profile/master/Profile.ps1"
-    Invoke-RestMethod -OutFile (Join-Path (Split-Path -Parent -Path $profile) "Cmdlets.ps1") -Uri "https://raw.githubusercontent.com/mariussm/powershell-profile/master/Cmdlets.ps1"
-    Invoke-RestMethod -OutFile (Join-Path (Split-Path -Parent -Path $profile) "FIMPowerShellModule.psm1") -Uri "https://raw.githubusercontent.com/mariussm/powershell-profile/master/Modules/FIMPowerShellModule.psm1"
+        [Parameter(Mandatory=$false,
+                   ValueFromPipelineByPropertyName=$false,
+                   Position=1)]
+        $Repository = "mariussm/powershell-profile"
+    )
+
+    Invoke-RestMethod -OutFile $profile -Uri "https://raw.githubusercontent.com/$Repository/$Branch/Profile.ps1"
+    Invoke-RestMethod -OutFile (Join-Path (Split-Path -Parent -Path $profile) "Cmdlets.ps1") -Uri "https://raw.githubusercontent.com/$Repository/$Branch/Cmdlets.ps1"
+    Invoke-RestMethod -OutFile (Join-Path (Split-Path -Parent -Path $profile) "FIMPowerShellModule.psm1") -Uri "https://raw.githubusercontent.com/$Repository/$Branch/Modules/FIMPowerShellModule.psm1"
     . $profile
 }
 
 . (Join-Path (Split-Path -Parent -Path $profile) "Cmdlets.ps1")
 Import-Module (Join-Path (Split-Path -Parent -Path $profile) "FIMPowerShellModule.psm1")
+
+if($env:USERPROFILE -and (pwd).Path.StartsWith("C:\Windows")) {
+    cd $env:USERPROFILE
+}

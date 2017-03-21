79f91888d7d471b13af2e0905f58b8f6486e3808
mariussm <marius.msm@gmail.com>
2017-03-21 08:20:05 +0100

    Added cmdlet

---
+++ Cmdlets/ConvertFrom-SAMLTokenToXML.ps1
@@ -0,0 +1,29 @@
+﻿function ConvertFrom-SAMLTokenToXML
+{
+    [CmdletBinding()]
+    [Alias()]
+    Param
+    (
+        # Param1 help description
+        [Parameter(Mandatory=$true,
+                   ValueFromPipeline=$true,
+                   Position=0)]
+        [String] $String
+    )
+
+    Begin
+    {
+    }
+    Process
+    {
+        if($String.Substring(0,1) -ne "<") {
+            Write-Verbose "Detected token as not saml, trying to convert from base64 first"
+            $String = ConvertFrom-Base64 $String
+        }
+
+        return ([xml] $String)
+    }
+    End
+    {
+    }
+}

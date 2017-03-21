5d0695aeaa94400865ab86b57d215aad37a3ac19
mariussm <marius.msm@gmail.com>
2017-03-21 13:24:34 +0100

    Added cmdlet  Get-AzureADDomainInfoFromPublicApi

---
+++ Cmdlets/Get-AzureADDomainInfoFromPublicApi.ps1
@@ -0,0 +1,25 @@
+﻿function Get-AzureADDomainInfoFromPublicApi
+{
+    [CmdletBinding()]
+    [Alias()]
+    Param
+    (
+        # Param1 help description
+        [Parameter(Mandatory=$true,
+                   ValueFromPipeline=$true,
+                   Position=0)]
+        [String] $Domain
+    )
+
+    Begin
+    {
+    }
+    Process
+    {
+        $Url = "https://login.microsoftonline.com/common/userrealm/?user=someone.random@" + $Domain + "&checkForMicrosoftAccount=true&api-version=2.1"
+        Invoke-RestMethod $Url
+    }
+    End
+    {
+    }
+}

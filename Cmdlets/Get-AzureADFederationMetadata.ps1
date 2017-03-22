779d1c0382c42e7eac4735a6751a4810bf2f2ebf
mariussm <marius.msm@gmail.com>
2017-03-22 07:53:31 +0100

    added azure federation metadata cmdlet

---
+++ Cmdlets/Get-AzureADFederationMetadata.ps1
@@ -0,0 +1,37 @@
+﻿<#
+.Synopsis
+    Returns the federation metadata as XML
+.DESCRIPTION
+    Returns the federation metadata as XML
+.EXAMPLE
+    Get-AzureADFederationMetadata "microsoft.com"
+#>
+function Get-AzureADFederationMetadata
+{
+    [CmdletBinding()]
+    [OutputType([xml])]
+    Param
+    (
+        [Parameter(Mandatory=$true,
+                ValueFromPipeline=$true,
+                Position=0)]
+        [String] $Domain,
+
+        [Parameter(Mandatory=$false,
+                ValueFromPipeline=$false,
+                Position=1)]
+        [String] $STS = "sts.windows.net"
+    )
+ 
+    Begin
+    {
+    }
+    Process
+    {
+        $XDocument = [System.Xml.Linq.XDocument]::Load( ("https://$STS/{0}/FederationMetadata/2007-06/FederationMetadata.xml" -f $Domain))
+        [xml] $XDocument
+    }
+    End
+    {
+    }
+}

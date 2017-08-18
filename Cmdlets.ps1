3437855cac46d8e013ff9ebb89ab937808f64679
mariussm <marius.msm@gmail.com>
2017-08-18 11:04:35 +0200

    Added Start-RdpFiles

--- Cmdlets.ps1
+++ Cmdlets.ps1
@@ -3538,6 +3538,28 @@ Function Split-String {
 
 }
 
+function Start-RdpFiles
+{
+    [CmdletBinding()]
+    [Alias()]
+    [OutputType([int])]
+    Param
+    (
+        [Parameter(Mandatory=$false,Position=0)]
+        [String] $Folder = (join-path ([System.Environment]::GetFolderPath("UserProfile")) "Downloads")
+    )
+
+    Begin
+    {
+    }
+    Process
+    {
+        dir $Folder -Recurse | ? Extension -eq ".rdp" | Out-GridView -OutputMode Multiple | ii
+    }
+    End
+    {
+    }
+}
 <#
  
 .SYNOPSIS
---
+++ Cmdlets/Start-RdpFiles.ps1
@@ -0,0 +1,22 @@
+﻿function Start-RdpFiles
+{
+    [CmdletBinding()]
+    [Alias()]
+    [OutputType([int])]
+    Param
+    (
+        [Parameter(Mandatory=$false,Position=0)]
+        [String] $Folder = (join-path ([System.Environment]::GetFolderPath("UserProfile")) "Downloads")
+    )
+
+    Begin
+    {
+    }
+    Process
+    {
+        dir $Folder -Recurse | ? Extension -eq ".rdp" | Out-GridView -OutputMode Multiple | ii
+    }
+    End
+    {
+    }
+}

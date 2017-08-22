e71f252a1a36ee0854c1a5ce7092d36a5c0397eb
mariussm <marius.msm@gmail.com>
2017-08-22 09:50:44 +0200

    Added Get-AzureADTenantId

--- Cmdlets.ps1
+++ Cmdlets.ps1
@@ -992,6 +992,42 @@ function Get-AzureADFederationMetadata
     {
     }
 }
+<#
+.Synopsis
+   Short description
+.DESCRIPTION
+   Long description
+.EXAMPLE
+   Example of how to use this cmdlet
+.EXAMPLE
+   Another example of how to use this cmdlet
+#>
+function Get-AzureADTenantId
+{
+    [CmdletBinding()]
+    [Alias()]
+    [OutputType([string])]
+    Param
+    (
+        # Param1 help description
+        [Parameter(Mandatory=$true,
+                   ValueFromPipelineByPropertyName=$true,
+                   Position=0)]
+        $DomainName
+    )
+
+    Begin
+    {
+    }
+    Process
+    {
+        $FederationMetadata = Get-AzureADFederationMetadata -Domain $DomainName
+        $FederationMetadata.EntityDescriptor.entityID -split "/" | where{$_ -match "^[a-zA-Z0-9]{8}-[a-zA-Z0-9]{4}-[a-zA-Z0-9]{4}-[a-zA-Z0-9]{4}-[a-zA-Z0-9]{12}$"}
+    }
+    End
+    {
+    }
+}
 Function Get-CodeSigningCertificate {
     [CmdletBinding()]
     [Alias()]

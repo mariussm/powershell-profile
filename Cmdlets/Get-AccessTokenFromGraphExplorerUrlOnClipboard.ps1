604a9088993b41b095c950b6965fb9f97850a675
Marius Solbakken Mellum <marius.msm@gmail.com>
2019-12-02 20:28:07 +0100

    Bugfix

--- Cmdlets/Get-AccessTokenFromGraphExplorerUrlOnClipboard.ps1
+++ Cmdlets/Get-AccessTokenFromGraphExplorerUrlOnClipboard.ps1
@@ -3,23 +3,54 @@ function Get-AccessTokenFromGraphExplorerUrlOnClipboard
     [CmdletBinding()]
     [Alias()]
     Param
-    ()
+    (
+        [Parameter(Mandatory=$false,
+                   ValueFromPipelineByPropertyName=$true,
+                   Position=0)]
+        [string[]] $RequiredScopes = @()
+    )
 
     Process
     {
         $first = $true
         do {
             if(!$first) {
-                Sleep -Seconds 1   
+                Start-Sleep -Seconds 1   
             }
             $first = $false 
 
-            Write-Verbose "Trying to get Graph Explorer URL from clipboard"
+            Write-Verbose "Trying to get Graph Explorer URL from clipboard, with requires scopes: $RequiredScopes" 
             $url = Get-Clipboard
-            if($url -ne $null -and $url.StartsWith("https://developer.microsoft.com/en-us/graph/graph-explorer#access_token=")) {
-                $token = $url -split "[=&]" | Select -Index 1
+            if(![string]::IsNullOrEmpty($url) -and $url.StartsWith("https://developer.microsoft.com/en-us/graph/graph-explorer#access_token=")) {
+                Write-Verbose "Found relevant url on the clipboard, starting verification"
+                $token = $url -split "[=&]" | Select-Object -Index 1
+
+                # Fix padding length for base 64
+                $token2 = $token.Split(".")[1] + [String]::new("=", 4 - ($token.Split(".")[1].Length % 4))
+                
+                # Converting from json
+                $tokenObject = [System.Text.Encoding]::UTF8.GetString(([System.Convert]::FromBase64String($token2))) | ConvertFrom-Json
+
+                # Checking for expiry (with 5 minutes minimum required)
+                $date = get-date "1/1/1970"
+                if((Get-Date).AddMinutes(5) -gt $date.AddSeconds($tokenObject.exp ).ToLocalTime()) {
+                    Write-Verbose "Token on clipboard is expired, not using it"
+                    $token = ""
+                }
+                
+                # Check scopes
+                if($RequiredScopes.Count -gt 0) {
+                    $tokenScopes = $tokenObject.scp.Split(" ")
+                    $RequiredScopes | Where-Object {$_ -notin $tokenScopes} | ForEach-Object {
+                        Write-Verbose "Token did not contain scope $($_), not using it"
+                        $token = ""
+                    }
+                }
+                
             }
-        } while($token -eq $null -or !$token.StartsWith("ey"))
+        } while([string]::IsNullOrEmpty($token) -or !$token.StartsWith("ey"))
+        
+        Write-Verbose "Token found"
         $token
     }
 }
